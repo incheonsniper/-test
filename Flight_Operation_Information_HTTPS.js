b0d7091b7b60a7a29cf4a1abf146acb4b609d5b6
junoo60 <tlswnsdn@outlook.com>
2026-01-16 18:20:24 +0900

    불필요한 파일 제거 및 프로젝트 정리

--- Flight_Operation_Information_HTTPS.js
+++ Flight_Operation_Information_HTTPS.js
@@ -5,14 +5,11 @@ const BasicKey = require('./authentication_key.json');
 const url = 'http://openapi.airport.co.kr/service/rest/AirportCodeList/getAirportCodeList';
 const queryParams = '?' + encodeURIComponent('serviceKey') + '=' + encodeURIComponent(BasicKey.key);
 
-// 함수 정의
 function getFlightStatus() {
   return axios.get(url + queryParams, { responseType: 'text' })
     .then(response => {
       const data = response.data;
-      // XML인지 JSON인지 판별
       if (data.trim().startsWith('<')) {
-        // XML 파싱
         return new Promise((resolve, reject) => {
           xml2js.parseString(data, { explicitArray: false }, (err, result) => {
             if (err) reject(err);
@@ -20,12 +17,8 @@ function getFlightStatus() {
           });
         });
       } else {
-        // JSON 그대로 반환
         return JSON.parse(data);
       }
     });
 }
-
-
-// 함수 내보내기
 module.exports = { getFlightStatus };
